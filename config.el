ea7ecf638e515865ded3a670eac5e523cb6ecb28
lem <jacobtophatleeming@gmail.com>
2020-07-02 08:38:29 +0100

    added quit other window function and bound it to 2

--- config.el
+++ config.el
@@ -50,7 +50,7 @@
 (setq dired-dwim-target t)
 
 (defun xah-dired-mode-setup()
-      (dired-hide-details-mode 1))
+  (dired-hide-details-mode 1))
 (add-hook 'dired-mode-hook 'xah-dired-mode-setup)
 
 (toggle-truncate-lines)
@@ -71,6 +71,11 @@
                              (float-time
                               (time-subtract after-init-time before-init-time)))
                      gcs-done)))
+
+(defun jacob-quit-other-window ()
+  (interactive)
+  (other-window 1)
+  (quit-window))
 
 (defun config-visit ()
   (interactive)
@@ -121,7 +126,8 @@
     (define-key xah-fly-key-map (kbd "a") 'counsel-M-x)
     (define-key xah-fly-key-map (kbd "n") 'swiper)
     (define-key xah-fly-key-map (kbd "8") 'er/expand-region)
-    (define-key xah-fly-key-map (kbd "4") 'jacob-split-window-below-select-new))
+    (define-key xah-fly-key-map (kbd "4") 'jacob-split-window-below-select-new)
+    (define-key xah-fly-key-map (kbd "2") 'jacob-quit-other-window))
 
   :config
   (define-prefix-command 'jacob-config-keymap)
@@ -152,7 +158,7 @@
         ("4" . jacob-split-window-right-select-new)))
 
 (setq w32-pass-rwindow-to-system nil
-	      w32-rwindow-modifier 'super)
+	  w32-rwindow-modifier 'super)
 
 (setq w32-pass-apps-to-system nil)
 (setq w32-apps-modifier 'hyper)
@@ -249,18 +255,18 @@
   :custom (gdscript-use-tab-indents nil))
 
 (use-package beacon
-      :ensure t
+  :ensure t
   :defer 2
-      :diminish
-      :config
-      (beacon-mode 1))
+  :diminish
+  :config
+  (beacon-mode 1))
 
 (use-package which-key
-      :ensure t
+  :ensure t
   :defer 2
-      :diminish
-      :config
-      (which-key-mode))
+  :diminish
+  :config
+  (which-key-mode))
 
 (use-package company
   :ensure t
@@ -290,15 +296,15 @@
   (key-chord-define xah-fly-key-map "f;" 'avy-goto-end-of-line))
 
 (use-package rainbow-mode
-      :ensure t
-      :diminish
-      :hook prog-mode)
+  :ensure t
+  :diminish
+  :hook prog-mode)
 
 (use-package dimmer
-      :ensure t
+  :ensure t
   :defer 5
-      :config
-      (dimmer-mode))
+  :config
+  (dimmer-mode))
 
 (use-package omnisharp
    :ensure t
@@ -348,8 +354,8 @@
   :mode ("\\.clj\\$" . clojure-mode))
 
 (use-package restart-emacs
-      :ensure t
-      :defer t)
+  :ensure t
+  :defer t)
 
 (use-package smex
   :ensure t
@@ -358,25 +364,25 @@
   ("M-x" . smex))
 
 (use-package diminish
-      :ensure t
-      :defer t
-      :config
-      (diminish 'subword-mode)
-      (diminish 'org-src-mode)
-      (diminish 'eldoc-mode))
+  :ensure t
+  :defer t
+  :config
+  (diminish 'subword-mode)
+  (diminish 'org-src-mode)
+  (diminish 'eldoc-mode))
 
 (use-package switch-window
-      :ensure t
-      :defer t
-      :config
-      (setq switch-window-input-style 'minibuffer)
-      (setq switch-window-threshold 2)
-      (setq switch-window-multiple-frames t)
-      (setq switch-window-shortcut-style 'qwerty)
-      (setq switch-window-qwerty-shortcuts
+  :ensure t
+  :defer t
+  :config
+  (setq switch-window-input-style 'minibuffer)
+  (setq switch-window-threshold 2)
+  (setq switch-window-multiple-frames t)
+  (setq switch-window-shortcut-style 'qwerty)
+  (setq switch-window-qwerty-shortcuts
 		'("q" "w" "e" "r" "a" "s" "d" "f" "z" "x" "c" "v"))
-      :bind
-      ([remap xah-next-window-or-frame] . switch-window))
+  :bind
+  ([remap xah-next-window-or-frame] . switch-window))
 
 (use-package ivy
   :ensure t
@@ -405,17 +411,17 @@
   :config (counsel-mode))
 
 (use-package multiple-cursors
-      :ensure t
-      :bind
-      (:map xah-fly-dot-keymap
+  :ensure t
+  :bind
+  (:map xah-fly-dot-keymap
 		("m" . jacob-multiple-cursors-keymap)
-      :map jacob-multiple-cursors-keymap
+  :map jacob-multiple-cursors-keymap
 		("l" . mc/edit-lines)
 		(">" . mc/mark-next-like-this)
 		("<" . mc/mark-previous-like-this)
 		("a" . mc/mark-all-like-this))
-      :init
-      (define-prefix-command 'jacob-multiple-cursors-keymap))
+  :init
+  (define-prefix-command 'jacob-multiple-cursors-keymap))
 
 (use-package expand-region
   :ensure t
@@ -445,18 +451,18 @@
         ("f" . jacob-shell-pop-shell)))
 
 (use-package move-text
-      :ensure t
-      :config
-      (move-text-default-bindings))
+  :ensure t
+  :config
+  (move-text-default-bindings))
 
 (use-package eshell-up
-      :ensure t)
+  :ensure t)
 
 (use-package langtool
-      ;; :ensure t
-      :defer t
-      :config
-      (setq langtool-language-tool-jar
+  ;; :ensure t
+  :defer t
+  :config
+  (setq langtool-language-tool-jar
 		"/home/lem/Documents/LanguageTool-4.8/languagetool-commandline.jar"))
 
 (use-package color-theme-sanityinc-tomorrow
