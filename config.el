5e29ce9e62b0615d34246dbff3205585e4fc8d6d
Jacob Leeming <jacobtophatleeming@gmail.com>
2020-07-13 08:15:43 +0100

    successful linting achieved!

--- config.el
+++ config.el
@@ -44,7 +44,7 @@
                      gcs-done)))
 
 (setq w32-pass-rwindow-to-system nil
-	  w32-rwindow-modifier 'super)
+	      w32-rwindow-modifier 'super)
 
 (setq w32-pass-apps-to-system nil)
 (setq w32-apps-modifier 'hyper)
@@ -246,6 +246,12 @@
   :config 
   (setq gdscript-use-tab-indents nil))
 
+(use-package flycheck
+  :ensure t
+  :defer 2
+  :config
+  (global-flycheck-mode))
+
 (setq lsp-keymap-prefix "s-l")
 
   (use-package lsp-mode
@@ -254,9 +260,10 @@
     (java-mode . lsp)
     (lsp-mode . lsp-enable-which-key-integration)
     :commands lsp
-    :config
+    :init
     (setq lsp-completion-enable-additional-text-edit nil)
-    (setq lsp-prefer-capf t))
+    (setq lsp-prefer-capf t)
+    (setq lsp-prefer-flymake nil))
 
   (use-package lsp-ui
     :ensure t
@@ -273,18 +280,18 @@
 (setq lsp-enable-links nil)
 
 (use-package beacon
-  :ensure t
+      :ensure t
   :defer 2
-  :diminish
-  :config
-  (beacon-mode 1))
+      :diminish
+      :config
+      (beacon-mode 1))
 
 (use-package which-key
-  :ensure t
+      :ensure t
   :defer 2
-  :diminish
-  :config
-  (which-key-mode))
+      :diminish
+      :config
+      (which-key-mode))
 
 (use-package company
   :ensure t
@@ -319,15 +326,15 @@
   (key-chord-define xah-fly-key-map "f;" 'avy-goto-end-of-line))
 
 (use-package rainbow-mode
-  :ensure t
-  :diminish
-  :hook prog-mode)
+      :ensure t
+      :diminish
+      :hook prog-mode)
 
 (use-package dimmer
-  :ensure t
+      :ensure t
   :defer 5
-  :config
-  (dimmer-mode))
+      :config
+      (dimmer-mode))
 
 (use-package omnisharp
    :ensure t
@@ -364,20 +371,14 @@
   :config
   (key-chord-mode 1))
 
-(use-package flycheck
-  :ensure t
-  :defer 2
-  :config
-  (global-flycheck-mode))
-
 (use-package cider
   :diminish
   :ensure t
   :mode ("\\.clj\\$" . clojure-mode))
 
 (use-package restart-emacs
-  :ensure t
-  :defer t)
+      :ensure t
+      :defer t)
 
 (use-package smex
   :ensure t
@@ -386,25 +387,25 @@
   ("M-x" . smex))
 
 (use-package diminish
-  :ensure t
-  :defer t
-  :config
-  (diminish 'subword-mode)
-  (diminish 'org-src-mode)
-  (diminish 'eldoc-mode))
+      :ensure t
+      :defer t
+      :config
+      (diminish 'subword-mode)
+      (diminish 'org-src-mode)
+      (diminish 'eldoc-mode))
 
 (use-package switch-window
-  :ensure t
-  :defer t
-  :config
-  (setq switch-window-input-style 'minibuffer)
-  (setq switch-window-threshold 2)
-  (setq switch-window-multiple-frames t)
-  (setq switch-window-shortcut-style 'qwerty)
-  (setq switch-window-qwerty-shortcuts
+      :ensure t
+      :defer t
+      :config
+      (setq switch-window-input-style 'minibuffer)
+      (setq switch-window-threshold 2)
+      (setq switch-window-multiple-frames t)
+      (setq switch-window-shortcut-style 'qwerty)
+      (setq switch-window-qwerty-shortcuts
 		'("q" "w" "e" "r" "a" "s" "d" "f" "z" "x" "c" "v"))
-  :bind
-  ([remap xah-next-window-or-frame] . switch-window))
+      :bind
+      ([remap xah-next-window-or-frame] . switch-window))
 
 (use-package ivy
   :ensure t
@@ -432,17 +433,17 @@
   :config (counsel-mode))
 
 (use-package multiple-cursors
-  :ensure t
-  :bind
-  (:map xah-fly-dot-keymap
+      :ensure t
+      :bind
+      (:map xah-fly-dot-keymap
 		("m" . jacob-multiple-cursors-keymap)
-  :map jacob-multiple-cursors-keymap
+      :map jacob-multiple-cursors-keymap
 		("l" . mc/edit-lines)
 		(">" . mc/mark-next-like-this)
 		("<" . mc/mark-previous-like-this)
 		("a" . mc/mark-all-like-this))
-  :init
-  (define-prefix-command 'jacob-multiple-cursors-keymap))
+      :init
+      (define-prefix-command 'jacob-multiple-cursors-keymap))
 
 (use-package expand-region
   :ensure t
@@ -473,18 +474,18 @@
         ("f" . jacob-shell-pop-shell)))
 
 (use-package move-text
-  :ensure t
-  :config
-  (move-text-default-bindings))
+      :ensure t
+      :config
+      (move-text-default-bindings))
 
 (use-package eshell-up
-  :ensure t)
+      :ensure t)
 
 (use-package langtool
-  ;; :ensure t
-  :defer t
-  :config
-  (setq langtool-language-tool-jar
+      ;; :ensure t
+      :defer t
+      :config
+      (setq langtool-language-tool-jar
 		"/home/lem/Documents/LanguageTool-4.8/languagetool-commandline.jar"))
 
 (use-package color-theme-sanityinc-tomorrow
