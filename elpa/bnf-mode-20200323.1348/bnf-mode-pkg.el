673b1ddca209e088d54fc97a3f35f3427389365d
lem <jacobtophatleeming@gmail.com>
2020-08-30 22:06:13 +0100

    bnf-mode added

---
+++ elpa/bnf-mode-20200323.1348/bnf-mode-pkg.el
@@ -0,0 +1,13 @@
+(define-package "bnf-mode" "20200323.1348" "Major mode for editing BNF grammars."
+  '((cl-lib "0.5")
+    (emacs "24.3"))
+  :commit "d88eef69ae66ea1ffa21a65317afe84c9ddb0814" :keywords
+  '("languages")
+  :authors
+  '(("Serghei Iakovlev" . "egrep@protonmail.ch"))
+  :maintainer
+  '("Serghei Iakovlev" . "egrep@protonmail.ch")
+  :url "https://github.com/sergeyklay/bnf-mode")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
