6a9b1b944d30a4b609504f78808ab521c6a9cd60
lem <jacobtophatleeming@gmail.com>
2020-10-09 19:52:34 +0100

    setup php

---
+++ elpa/company-php-20200916.751/company-php-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "company-php" "20200916.751" "A company back-end for PHP." '((cl-lib "0.5") (ac-php-core "2.0") (company "0.9")) :commit "7c023b9ced156cee03171e16c3dac6d26923042f" :keywords '("completion" "convenience" "intellisense") :authors '(("jim" . "xcwenn@qq.com")) :maintainer '("jim") :url "https://github.com/xcwen/ac-php")
---
+++ elpa/xcscope-20200828.1027/xcscope-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "xcscope" "20200828.1027" "cscope interface for (X)Emacs" 'nil :commit "b9bd846f0c9dcc8acfe7d1046eafc3d0e87473ad" :keywords '("languages" "c") :authors '(("Darryl Okahata" . "darrylo@sonic.net") ("Dima Kogan" . "dima@secretsauce.net")) :maintainer '("Dima Kogan" . "dima@secretsauce.net") :url "https://github.com/dkogan/xcscope.el")
