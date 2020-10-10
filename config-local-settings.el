061f2a1c266427d65ada35b24f155370ab093b79
lem <jacobtophatleeming@gmail.com>
2020-10-10 09:40:12 +0100

    created new variable set up for individual configs

---
+++ config-local-settings.el
@@ -0,0 +1,12 @@
+;; This file exists to set local settings for each computer using this configuration.
+;; Things like paths to utilities, or fonts and font sizes.
+;; When pulling
+;; location of utilities
+
+(defconst jacob-windows-cmd-file-path "")
+(defconst jacob-omnisharp-file-path "D:/Programming/OmniSharp/omnisharp-roslyn/bin/Debug/OmniSharp.Stdio.Driver/net472/OmniSharp.exe")
+(defconst jacob-java-language-server-file-path "")
+
+;; Font settings
+(defconst jacob-font-size 12 "The font size for this computer's emacs configuration. Change the value in ~/.emacs.d/config-local-settings.el to suit the computer in use. If value is 0, then the default font size will be used.")
+(defconst jacob-font-family nil "The font family for this computer's emacs configuration. Change the value in ~/.emacs.d/config-local-settings.el to suit the computer in use. If value is 0, then the default font family will be used.")
