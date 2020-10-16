c024d00cfffa204b6c14012dfa552283cd84f1d2
lem <jacobtophatleeming@gmail.com>
2020-10-16 20:12:52 +0100

    tweaked shell pop to hopefully work better cross platform

--- init.el
+++ init.el
@@ -723,7 +723,7 @@
 
   :config
   (setq shell-pop-autocd-to-working-dir nil)
-  (setq shell-pop-shell-type (quote ("eshell" "*eshell*" (lambda nil (eshell)))))
+  ;; (setq shell-pop-shell-type (quote ("eshell" "*eshell*" (lambda nil (eshell)))))
   (setq shell-pop-universal-key "<H-return>")
   (setq shell-pop-window-position "bottom")
   (setq shell-pop-window-size 50)
@@ -737,12 +737,16 @@
 
   (defun jacob-shell-pop-shell ()
     (interactive)
-    (let ((shell-file-name "C:/Windows/System32/Cmd.exe")
+    (let ((shell-file-name (cond
+                            ((string-equal system-type "windows-nt")
+                             "C:/Windows/System32/Cmd.exe")
+                            ((string-equal system-type "gnu/linux")
+                             "/bin/sh")))
           (shell-pop-shell-type '("shell" "*shell*" (lambda () (shell))))
           (shell-pop-term-shell "shell"))
       (shell-pop--set-shell-type 'shell-pop-shell-type shell-pop-shell-type)
       (call-interactively 'shell-pop)))
-
+  
   :bind
   (:map xah-fly-n-keymap
         ("d" . jacob-shell-pop-eshell)
