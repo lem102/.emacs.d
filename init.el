6f91fc7c1a9c6f09c395216131874ac9ca6ddc66
Jacob Leeming <jacobtophatleeming@gmail.com>
2020-10-19 22:22:32 +0100

    added better integration for eglot with java mode

--- init.el
+++ init.el
@@ -409,7 +409,18 @@
 ;; will eventually be quite a busy section.
 ;; ** eglot
 (use-package eglot
-  :ensure t)
+  :ensure t
+  :config
+  (defconst jacob-eclipse-jdt-home "/home/lem/Documents/java/eclipse.jdt.ls/plugins/org.eclipse.equinox.launcher_1.5.700.v20200207-2156.jar")
+
+  (defun jacob-eglot-eclipse-jdt-contact (interactive)
+    "Contact with the jdt server input INTERACTIVE."
+    (let ((cp (getenv "CLASSPATH")))
+      (setenv "CLASSPATH" (concat cp ":" jacob-eclipse-jdt-home))
+      (unwind-protect (eglot--eclipse-jdt-contact nil)
+        (setenv "CLASSPATH" cp))))
+  (setcdr (assq 'java-mode eglot-server-programs) #'jacob-eglot-eclipse-jdt-contact)
+  :hook (java-mode-hook . eglot-ensure))
 ;; ** Base lsp-mode
 ;; *** lsp-mode
 (use-package lsp-mode
