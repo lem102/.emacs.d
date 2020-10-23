605a46f0a9f034f945e797c4c4c0a329e56b9d47
Jacob Leeming <jacobtophatleeming@gmail.com>
2020-10-23 09:18:54 +0100

    made sure web-mode is loaded for php blade templates

--- init.el
+++ init.el
@@ -545,14 +545,16 @@
 
   :config
   (setq web-mode-engines-alist
-        '(("razor"	. "\\.cshtml\\'")))
+        '(("razor"	. "\\.cshtml\\'")
+          ("blade" . "\\.blade.php\\'")))
   (setq web-mode-markup-indent-offset 2)
   (setq web-mode-css-indent-offset 2)
   (setq web-mode-code-indent-offset 2)
 
   :hook (web-mode-hook . jacob-web-mode-config)
 
-  :mode (("\\.html?\\'" . web-mode)
+  :mode (("\\.blade.php\\'" . web-mode)
+         ("\\.html?\\'" . web-mode)
          ("\\.cshtml\\'" . web-mode)
          ("\\.css\\'" . web-mode)))
 ;; ** json-mode
