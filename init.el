b424ee46040fddededf128efcbe421c9d68a17cb
lem <jacobtophatleeming@gmail.com>
2020-10-30 00:36:21 +0000

    fixed misnamed function

--- init.el
+++ init.el
@@ -13,7 +13,7 @@
 (defun doom-defer-garbage-collection-h ()
   (setq gc-cons-threshold most-positive-fixnum))
 
-(defun doom-restore-geabage-collection ()
+(defun doom-restore-garbage-collection-h ()
   (run-at-time
    1 nil (lambda () (setq gc-cons-threshold 16777216))))
 
