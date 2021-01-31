1b759bfc9eaf77cec348bd246356a090a129687e
Jacob Leeming <jacobtophatleeming@gmail.com>
2021-01-31 20:10:07 +0000

    added pulse to some more movement commands.

--- jacob-init/jacob-init-pulse.el
+++ jacob-init/jacob-init-pulse.el
@@ -7,5 +7,8 @@
 (dolist (command '(recenter-top-bottom
                    scroll-up-command
                    scroll-down-command
-                   other-window))
+                   other-window
+                   ace-window
+                   avy-goto-char
+                   avy-goto-end-of-line))
   (advice-add command :after #'jacob-pulse-line))
