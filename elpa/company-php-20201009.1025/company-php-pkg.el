6d7cd15739c3d53bfe64dbe6ecc8afbcf977b96f
Jacob Leeming <jacobtophatleeming@gmail.com>
2021-01-31 14:11:49 +0000

    removed php packages

--- elpa/company-php-20201009.1025/company-php-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "company-php" "20201009.1025" "A company back-end for PHP." '((cl-lib "0.5") (ac-php-core "2.0") (company "0.9")) :commit "4712911b74fd5290b74ca5e8d9cee0609d2bada3" :keywords '("completion" "convenience" "intellisense") :authors '(("jim" . "xcwenn@qq.com")) :maintainer '("jim") :url "https://github.com/xcwen/ac-php")
