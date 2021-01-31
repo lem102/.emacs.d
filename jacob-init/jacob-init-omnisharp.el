1df2551d56b72b2511dd1b3e0c6ff550d80fca33
Jacob Leeming <jacobtophatleeming@gmail.com>
2021-01-31 19:20:52 +0000

    tweaked config for csharp mode and omnisharp

--- jacob-init/jacob-init-omnisharp.el
+++ jacob-init/jacob-init-omnisharp.el
@@ -1,5 +1,3 @@
-;; ** omnisharp
-;; FIXME: if company mode is not started before csharp mode is entered, omnisharp mode will not activate
 (use-package omnisharp
   :ensure t
   :defer t
@@ -14,26 +12,12 @@
         ("o" . omnisharp-start-omnisharp-server)
         ("O" . omnisharp-stop-server))
   :config
-  ;; at this point, company mode is enabled.
   (define-prefix-command 'jacob-omnisharp-keymap)
   (define-key xah-fly-dot-keymap (kbd "o") jacob-omnisharp-keymap)
-
   (setq omnisharp-company-ignore-case nil)
-  (setq omnisharp-server-executable-path (expand-file-name jacob-omnisharp-file-path))
+  (if (boundp jacob-omnisharp-file-path)
+      (setq omnisharp-server-executable-path (expand-file-name jacob-omnisharp-file-path))))
 
-  (defun jacob-csharp-indent-or-complete ()
-    (interactive)
-    (if (region-active-p)
-        (c-indent-line-or-region :region (region-bounds))
-      (let ((old-point (point)))
-        (c-indent-line-or-region)
-        (if (eq old-point (point))
-            (call-interactively 'counsel-company)))))
-
-  (define-key csharp-mode-map (kbd "<tab>") 'jacob-csharp-indent-or-complete)
-  :hook (csharp-mode-hook . omnisharp-mode))
-
-;; *** add omnisharp to company backend
 (use-package emacs
   :after company omnisharp
   :config
