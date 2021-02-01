a9491483e25a62e62f1e7f3540438cffe558ae50
Jacob Leeming <jacobtophatleeming@gmail.com>
2021-02-01 20:56:25 +0000

    removed use package from jacob-init-misc. split settings into sections

--- jacob-init/jacob-init-misc.el
+++ jacob-init/jacob-init-misc.el
@@ -1,86 +1,72 @@
-;; don't ask for conformation when killing buffers with an attached process
-(use-package emacs
-  :config
-  (setq kill-buffer-query-functions (delq 'process-kill-buffer-query-function kill-buffer-query-functions))
-  ;; this is for asking on exit
-  (setq confirm-kill-processes nil))
-(use-package recentf
-  :commands recentf-open-files
-  :config
-  (recentf-mode 1))
-;; ** dabbrev
-(use-package dabbrev
-  :config
+(setq kill-buffer-query-functions (delq 'process-kill-buffer-query-function kill-buffer-query-functions))
+(setq read-process-output-max (* 1024 1024))
+(setq ring-bell-function 'ignore)
+(setq auto-window-vscroll nil)
+(setq scroll-conservatively 100)
+(setq create-lockfiles nil)
+(setq history-length 1000)
+(setq history-delete-duplicates t))
+
+(require 'files)
+(with-eval-after-load 'files
+  (setq confirm-kill-processes nil)
+  (setq backup-by-copying t)
+  (setq make-backup-files nil)
+  (setq auto-save-default nil))
+
+(require 'recentf)
+(with-eval-after-load 'recentf
+  (add-hook 'after-init-hook #'recentf-mode))
+
+(require 'dabbrev)
+(with-eval-after-load 'dabbrev
   (setq dabbrev-case-fold-search nil)
   (setq dabbrev-case-replace nil))
-;; ** put custom stuff in custom.el
-(use-package cus-edit
-  :defer 0.1
-  :config
-  (setq custom-file "~/.emacs.d/custom.el"))
-;; ** prevent emacs from disabling commands.
-(use-package novice
-  :defer 0.1
-  :config
+
+(require 'cus-edit)
+(with-eval-after-load 'cus-edit
+  (setq custom-file (make-temp-file "emacs-custom-")))
+
+(require 'novice)
+(with-eval-after-load 'novice
   (setq disabled-command-function nil))
-;; ** move by logical lines
-(use-package simple
-  :defer 0.1
-  :config
+
+(require 'simple)
+(with-eval-after-load 'simple
   (setq line-move-visual t))
-;; ** Use ls implemented in elisp to have consistent behaviour across platforms
+
+;; TODO: figure out how to group these when on windows
 (setq ls-lisp-use-insert-directory-program nil)
 (setq ls-lisp-dirs-first t)
-;; ** Read more data from process
-(setq read-process-output-max (* 1024 1024))
-;; ** Reduce lag
-(setq auto-window-vscroll nil)
-(setq redisplay-dont-pause t) ; obsolete
-;; ** Stop the bell ringing all the time
-(setq ring-bell-function 'ignore)
-;; ** Prevent the view of the screen jumping to the middle when scrolling out of the view.
-(setq scroll-conservatively 100)
-;; ** subword-mode
-(use-package subword
-  :defer 0.1
-  :config (global-subword-mode))
 
-;; ** Make typing delete/overwrite region
-(use-package delsel
-  :defer 0.1
-  :config
-  (delete-selection-mode 1))
+(require 'subword)
+(with-eval-after-load 'subword
+  (add-hook 'after-init-hook #'global-subword-mode))
 
-;; ** Turn on bracket match highlight
-(use-package paren
-  :defer 0.1
-  :config
-  (show-paren-mode 1))
-;; ** Prevent emacs from creating debris when editing files
-(use-package emacs
-  :config
-  (setq make-backup-files nil)
-  (setq auto-save-default nil)
-  (setq create-lockfiles nil))
-;; ** Backups don't destroy original file's creation date
-(setq backup-by-copying t)
+(require 'delsel)
+(with-eval-after-load 'delsel
+  (add-hook 'after-init-hook #'delete-selection-mode))
 
-;; ** Y/n instead of yes/no
+(require 'paren)
+(with-eval-after-load 'paren
+  (setq show-paren-style 'parenthesis)
+  (setq show-paren-when-point-in-periphery nil)
+  (setq show-paren-when-point-inside-paren nil)
+  (add-hook 'after-init-hook #'show-paren-mode))
+
 (defalias 'yes-or-no-p 'y-or-n-p)
 
-;; ** Expert mode for ibuffer
-(setq ibuffer-expert t)
-;; ** Save minibuffer history (useful for compile)
-(use-package savehist
-  :defer 0.1
-  :config
+(require 'ibuffer)
+(with-eval-after-load 'ibuffer
+  (setq ibuffer-expert t))
+
+(require 'savehist)
+(with-eval-after-load 'savehist
   (setq savehist-file "~/.emacs.d/savehist")
-  (setq history-length 1000)
-  (setq history-delete-duplicates t)
   (setq savehist-save-minibuffer-history t))
-;; ** Save cursor position in files
-(use-package saveplace
-  :config
+
+(require 'saveplace)
+(with-eval-after-load 'saveplace
   (setq save-place-file "~/.emacs.d/saveplace")
   (setq save-place-forget-unreadable-files t)
-  (save-place-mode 1))
+  (add-hook 'after-init-hook #'save-place-mode))
