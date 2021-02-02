9e189ecb931372dc3489ea9b8eefe8f5f9aabcd1
Jacob Leeming <jacobtophatleeming@gmail.com>
2021-02-02 08:37:41 +0000

    made greater use of use-package where possible

--- jacob-init/jacob-init-xah-fly-keys.el
+++ jacob-init/jacob-init-xah-fly-keys.el
@@ -13,11 +13,12 @@
 
   (defun jacob-xah-command-binds ()
     "Set custom keys for xah-fly-keys keybindings."
-    (define-key xah-fly-command-map (kbd "s") 'enter)
-    (define-key xah-fly-command-map (kbd "8") 'er/expand-region)
-    (define-key xah-fly-command-map (kbd "4") 'jacob-split-window-below-select-new)
-    (define-key xah-fly-command-map (kbd "2") 'jacob-quit-popup-window)
-    (define-key xah-fly-command-map (kbd ",") #'ace-window))
+    (let ((map xah-fly-command-map))
+      (define-key map (kbd "s") 'enter)
+      (define-key map (kbd "8") 'er/expand-region)
+      (define-key map (kbd "4") 'jacob-split-window-below-select-new)
+      (define-key map (kbd "2") 'jacob-quit-popup-window)
+      (define-key map (kbd ",") #'ace-window)))
 
   :config
   (load-file (expand-file-name "~/.emacs.d/myLisp/jacob-xah-modified-commands.el"))
@@ -26,19 +27,24 @@
   (xah-fly-keys-set-layout "qwerty")
   (xah-fly-keys 1)
 
-  (add-hook 'xah-fly-command-mode-activate-hook 'jacob-xah-command-binds)
-  (jacob-xah-command-binds) ;; call it on startup so binds are set without calling xah-fly-command-mode-activate first.
-
   (key-chord-define-global "fd" 'xah-fly-command-mode-activate)
   (key-chord-define-global "fj" 'avy-goto-char)
   (key-chord-define-global "fk" 'avy-goto-end-of-line)
-  (add-hook 'minibuffer-setup-hook 'xah-fly-insert-mode-activate)
-  (add-hook 'minibuffer-exit-hook 'xah-fly-command-mode-activate)
 
-  (define-key xah-fly-dot-keymap (kbd "p") project-prefix-map)
-  (define-key xah-fly-dot-keymap (kbd "v") vc-prefix-map)
-  
+  ;; TODO: figure out how to bind these using use-package
+  (let ((map xah-fly-dot-keymap))
+    (define-key map (kbd "p") project-prefix-map)
+    (define-key map (kbd "v") vc-prefix-map))
+
+  :hook
+  (xah-fly-keys-hook . jacob-xah-command-binds)
+  (xah-fly-command-mode-activate-hook . jacob-xah-command-binds)
+  (minibuffer-setup-hook . xah-fly-insert-mode-activate)
+  (minibuffer-exit-hook . xah-fly-command-mode-activate)
   :bind
+  (:map xah-fly-dot-keymap
+        ("c" . jacob-config-keymap)
+        ("s" . consult-line))
   (:map jacob-config-keymap
         ("r" . jacob-config-reload)
         ("R" . restart-emacs)
@@ -63,9 +69,6 @@
         ("c" . jacob-insert-hash)
         ("d" . backspace)
         ("v" . jacob-insert-tilde))
-  (:map xah-fly-dot-keymap
-        ("c" . jacob-config-keymap)
-        ("s" . consult-line))
   (:map xah-fly-leader-key-map
         ("4" . jacob-split-window-right-select-new)
         ("v" . consult-yank))
