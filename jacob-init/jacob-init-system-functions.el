6045bdb15b4d20d75cee8bcfb0f0e39048dc324b
lem <jacobtophatleeming@gmail.com>
2021-02-07 08:38:17 +0000

    removed accidental function call

--- jacob-init/jacob-init-system-functions.el
+++ jacob-init/jacob-init-system-functions.el
@@ -4,6 +4,6 @@
 If user inputs yes, system is shutdown. Otherwise, nothing happens."
   (interactive)
   (if (yes-or-no-p "Shutdown system?")
-      (shell-command-to-string "pwsh -Command Stop-Computer")))
+      (shell-command "pwsh -Command Stop-Computer")))
 
 ;; https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_pwsh?view=powershell-7.1
