77e99f155d59e9cd2d05c25df9118e7a9108d1b5
Jacob Leeming <jacobtophatleeming@gmail.com>
2021-02-17 22:10:41 +0000

    updated packages

--- elpa/csharp-mode-20201013.811/csharp-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "csharp-mode" "20201013.811" "C# mode derived mode" 'nil :commit "217d3a7b93427a68a08903aab9bed320f0a80736" :keywords '("c#" "languages" "oop" "mode") :authors '(("Theodor Thornhill" . "theo@thornhill.no")) :maintainer '("Jostein Kjønigsen" . "jostein@gmail.com") :url "https://github.com/josteink/csharp-mode")
