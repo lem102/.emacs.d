fd54d265f9a4595c7870624c9230980f8a496d99
lem <jacobtophatleeming@gmail.com>
2021-03-10 10:57:05 +0000

    configured org babel to use octave

--- jacob-init/jacob-init-org-mode.el
+++ jacob-init/jacob-init-org-mode.el
@@ -11,8 +11,11 @@
   (add-hook 'org-babel-post-tangle-hook 'jacob-org-babel-tangle-delete-newline)
 
   (setq-default yas-indent-line 'auto)  ; used to be set to 'fixed, not sure why. auto allows for us to not worry about indenting when we are doing else if.
-  (add-to-list 'org-structure-template-alist
-               '("el" "#+BEGIN_SRC emacs-lisp\n?\n#+END_SRC"))
+  ;; (add-to-list 'org-structure-template-alist
+  ;;              '("el" "#+BEGIN_SRC emacs-lisp\n?\n#+END_SRC"))
 
   (setq org-latex-pdf-process (list "latexmk -pdf %f")) ; probably requires texlive
-  )
+
+  (org-babel-do-load-languages
+   'org-babel-load-languages
+   '((octave . t))))
