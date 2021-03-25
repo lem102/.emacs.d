89d976de66d4f76317bc1d511e9ae34d79dfd2a9
Jacob Leeming <jacobtophatleeming@gmail.com>
2021-03-25 21:24:58 +0000

    added avy dispatch to capitalize word

--- jacob-init/jacob-init-avy.el
+++ jacob-init/jacob-init-avy.el
@@ -2,11 +2,16 @@
   :ensure t
   :config
 
+  (defun jacob-avy-action-xah-toggle-letter-case (pt)
+    (goto-char pt)
+    (xah-toggle-letter-case))
+  
   (setq avy-style 'at)
   (setq avy-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l ?\;))
   (setq avy-orders-alist '((avy-goto-end-of-line . avy-order-closest)
                            (avy-goto-word-or-subword-1 . avy-order-closest)))
-  (setq avy-dispatch-alist '((?x . avy-action-teleport)
+  (setq avy-dispatch-alist '((?b . jacob-avy-action-xah-toggle-letter-case)
+                             (?x . avy-action-teleport)
                              (?t . avy-action-mark)
                              (?c . avy-action-copy)
                              (?v . avy-action-yank)
