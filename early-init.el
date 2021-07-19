9b741411a07212fc0dbff79e3d12b60266365421
Jacob Leeming <jacobtophatleeming@gmail.com>
2021-07-19 09:34:19 +0100

    solved the dired-x issue, tweaked gc

--- early-init.el
+++ early-init.el
@@ -3,3 +3,12 @@
     (scroll-bar-mode 0))
 (menu-bar-mode 0)
 
+;; garbage collection
+
+(setq gc-cons-threshold most-positive-fixnum)
+(setq gc-cons-percentage 0.6)
+
+(add-hook 'emacs-startup-hook
+          (lambda ()
+            (setq gc-cons-threshold 16777216) ; 16mb
+            (setq gc-cons-percentage 0.1)))
