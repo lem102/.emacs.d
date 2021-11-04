2259e5f2cf5b28ff5189d796f63cd0e7615d542a
Jacob Leeming <jacobtophatleeming@gmail.com>
2021-11-04 13:16:51 +0000

    altered modeline slightly

--- init.el
+++ init.el
@@ -129,10 +129,12 @@
 
 ;; mode line
 
+(column-number-mode 1)
+(line-number-mode 1)
 (setq-default mode-line-format (list "%*" ; saved, readonly
                                      "%m: " ; major mode
                                      "%b " ; buffer name
-                                     "(%l,%c)" ; position of point
+                                     mode-line-position
                                      ))
 
 
