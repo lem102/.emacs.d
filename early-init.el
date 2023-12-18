54f5f43109614a00bef3d34efe14f26a1b989f00
Jacob Leeming <jacob.leeming@tappit.com>
2023-12-18 10:08:45 +0000

    cleanup early init

--- early-init.el
+++ early-init.el
@@ -1,11 +1,7 @@
-;; gui setup
-
 (unless (display-graphic-p)
   (scroll-bar-mode 0)
   (tool-bar-mode 0))
 (menu-bar-mode 0)
-
-;; garbage collection
 
 (setq gc-cons-threshold most-positive-fixnum)
 (setq gc-cons-percentage 0.6)
@@ -15,5 +11,4 @@
             (setq gc-cons-threshold 16777216) ; 16mb
             (setq gc-cons-percentage 0.1)))
 
-;; (setq package-enable-at-startup nil)
 (setq package-quickstart t)
