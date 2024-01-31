be95c78d0b6ee9fab1a8c3f03a03a05dcd38127d
Jacob Leeming <jacob.leeming@tappit.com>
2024-01-31 09:00:01 +0000

    function for setting PGPASSWORD when connecting to database

--- init.el
+++ init.el
@@ -580,6 +580,23 @@ See `sql-interactive-mode-hook' and `sql-product-alist'."
 
 (add-hook 'sql-interactive-mode-hook 'jacob-sql-interactive-mode-hook)
 
+(defun jacob-sql-connect (connection &optional buf-name)
+  "Wrapper for sql connect to set postgres password."
+  (interactive
+   (if sql-connection-alist
+       (list (sql-read-connection "Connection: ")
+             current-prefix-arg)
+     (user-error "No SQL Connections defined")))
+  (if (not (null (getenv "PGPASSWORD")))
+      (message "postgres password already set, aborting.")
+    (setenv "PGPASSWORD"
+            (cadr (assoc 'sql-password
+                         (assoc-string connection
+                                       sql-connection-alist
+                                       t))))
+    (sql-connect connection buf-name)
+    (setenv "PGPASSWORD")))
+
 
 ;; docview config
 
