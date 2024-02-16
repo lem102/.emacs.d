e1dd98cb2c3b04b8b2d7cef4c84664f02290d934
Jacob Leeming <jacob.leeming@tappit.com>
2024-02-16 15:09:32 +0000

    upgrade dape

--- elpa/dape-0.2.2/dape-pkg.el
+++
@@ -1,2 +0,0 @@
-;; Generated package description from dape.el  -*- no-byte-compile: t -*-
-(define-package "dape" "0.2.2" "Debug Adapter Protocol for Emacs" '((emacs "29.1")) :commit "a8e56d3767e1b063f6c49a6081ca4e22afdfaaaf" :maintainer '("Daniel Pettersson" . "daniel@dpettersson.net") :url "https://github.com/svaante/dape")
