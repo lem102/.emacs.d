bb39ebbc07ac9dbf2847685322709fb3079a12b2
Jacob Leeming <jacobtophatleeming@gmail.com>
2026-01-18 01:11:15 +0000

    x font

--- custom.el
+++ custom.el
@@ -69,4 +69,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((((type w32)) (:inherit nil :extend nil :stipple nil :background "#0d0e1c" :foreground "#ffffff" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight regular :height 113 :width normal :foundry "outline" :family "Consolas")))))
+ '(default ((((type w32)) (:inherit nil :extend nil :stipple nil :background "#0d0e1c" :foreground "#ffffff" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight regular :height 113 :width normal :foundry "outline" :family "Consolas"))
+            (((type x pgtk)) (:inherit nil :extend nil :stipple nil :background "#002451" :foreground "#ffffff" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight regular :height 113 :width normal :foundry "PfEd" :family "DejaVu Sans Mono")))))
+
