177de39b2062b619b4321a79b01655ea7991733e
Jacob Leeming <jacobtophatleeming@gmail.com>
2026-01-27 13:22:44 +0000

    setup gdscript formatter

--- lisp/jacob-apheleia.el
+++ lisp/jacob-apheleia.el
@@ -12,7 +12,16 @@
   (add-to-list 'apheleia-mode-alist '(csharp-ts-mode . csharpier))
 
   (add-to-list 'apheleia-formatters '(scalafmt "scalafmt" "--stdin" "--non-interactive" "--quiet" "--stdout"))
+
   (add-to-list 'apheleia-mode-alist '(scala-ts-mode . scalafmt))
+
+  (add-to-list 'apheleia-formatters '(gdscript-formatter "gdscript-formatter"))
+
+  (setf (alist-get 'gdscript-mode apheleia-mode-alist)
+        'gdscript-formatter)
+
+  (setf (alist-get 'gdscript-ts-mode apheleia-mode-alist)
+        'gdscript-formatter)
 
   (add-to-list 'apheleia-skip-functions #'jacob-apheleia-skip-function))
 
