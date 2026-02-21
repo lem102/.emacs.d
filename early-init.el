d92d51895e45a1f37e458efa87581592194e0abb
Jacob Leeming <jacobtophatleeming@gmail.com>
2026-02-21 18:50:25 +0000

    set some touchscreen options

--- early-init.el
+++ early-init.el
@@ -4,9 +4,7 @@
 (when (eq system-type 'android)
   (setenv "PATH" (format "%s:%s" "/data/data/com.termux/files/usr/bin"
 		                 (getenv "PATH")))
-  (push "/data/data/com.termux/files/usr/bin" exec-path)
-
-  (setopt touch-screen-display-keyboard t))
+  (push "/data/data/com.termux/files/usr/bin" exec-path))
 
 ;; Local Variables:
 ;; flymake-mode: nil
