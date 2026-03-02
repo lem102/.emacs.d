586e237d6e430f9736a7d00dca4581aed2875526
lem102 <jacobtophatleeming@gmail.com>
2026-03-02 09:26:00 +0000

    move all csharp code into one file

--- lisp/jacob-csharp-mode.el
+++ lisp/jacob-csharp-mode.el
@@ -537,6 +537,41 @@ which performs the deletion."
             (funcall f)))
         t))))
 
+;; TODO: decide what to do with this function.
+(defun jacob-csharp-move-class ()
+  "Move the class, record or interface at point to a new file."
+  (interactive)
+  (let* ((declaration-node (treesit-parent-until (treesit-node-at (point))
+                                                 (lambda (node)
+                                                   "Find a class, record, enum or interface node."
+                                                   (string-match-p
+                                                    "\\(class\\|record\\|enum\\|interface\\)_declaration"
+                                                    (treesit-node-type node)))
+                                                 t))
+         (declaration-code (treesit-node-text declaration-node))
+         (declaration-name (treesit-node-text
+                            (car
+                             (treesit-query-capture declaration-node
+                                                    '((record_declaration name: (identifier) @identifier))
+                                                    nil
+                                                    nil
+                                                    "NODE_ONLY"))
+                            "NO_PROPERTIES"))
+         (new-file-name (read-file-name "Move file to: "
+                                        nil
+                                        nil
+                                        nil
+                                        (concat declaration-name
+                                                ".cs"))))
+    (ignore-errors
+      (make-directory (file-name-directory new-file-name "PARENTS")))
+    (save-window-excursion
+      (find-file new-file-name)
+      (insert declaration-code)
+      (jacob-csharp-fix-namespace))
+    (delete-region (treesit-node-start declaration-node)
+                   (treesit-node-end declaration-node))))
+
 (defun jacob-csharp-mode-config ()
   "Configuration for `csharp-mode'."
   ;; TODO: include only my modifications rather than the whole data structure
