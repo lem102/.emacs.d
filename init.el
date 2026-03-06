96b4715ebcf572ce07434dfb4bbb39d6c781c855
lem102 <jacobtophatleeming@gmail.com>
2026-03-06 13:12:13 +0000

    move avy utilities into separate file

--- init.el
+++ init.el
@@ -847,63 +847,11 @@ then remove this function from `find-file-hook'."
   (add-to-list 'auto-mode-alist '("Directory.Packages.props" . nxml-mode)))
 
 (use-package avy
-  :defer t
-  :init
-  (keymap-global-set "M-j" #'avy-goto-char-timer)
-  (keymap-set isearch-mode-map "M-j" #'avy-isearch)
+  :bind (("M-j" . avy-goto-char-timer)
+         :map isearch-mode-map
+         ("M-j" . avy-isearch))
   :config
-  (defun jacob-avy-xref (point)
-    "Call `xref-find-definitions' at POINT."
-    (goto-char point)
-    (call-interactively #'xref-find-definitions)
-    (jacob-avy-go-home))
-
-  (defun jacob-avy-kill-line (point)
-    "Kill line at POINT."
-    (save-excursion
-      (goto-char point)
-      (puni-kill-line))
-    (jacob-avy-go-home))
-
-  (defun jacob-avy-copy-line (point)
-    "Copy line at POINT."
-    (save-excursion
-      (goto-char point)
-      (copy-region-as-kill (line-beginning-position) (line-end-position)))
-    (jacob-avy-go-home))
-
-  (defun jacob-avy-yank-line (pt)
-    "Copy sexp starting on PT."
-    (jacob-avy-copy-line pt)
-    (yank))
-
-  (defun jacob-avy-go-home ()
-    "Return to the avy origin."
-    (let ((dat (ring-ref avy-ring 0)))
-      (select-frame-set-input-focus
-       (window-frame (cdr dat)))
-      (select-window (cdr dat))
-      (goto-char (car dat))))
-
-  (defun jacob-avy-eglot-rename (pt)
-    "Copy sexp starting on PT."
-    (save-excursion
-      (goto-char pt)
-      (call-interactively #'eglot-rename))
-    (jacob-avy-go-home))
-
-  (defun jacob-avy-embark (pt)
-    "Call embark at PT."
-    (unwind-protect
-        (save-excursion
-          (goto-char pt)
-          (embark-act))
-      (jacob-avy-go-home)))
-
-  (defun jacob-avy-mark-to-char (pt)
-    "Set mark at PT."
-    (activate-mark)
-    (goto-char pt)))
+  (require 'jacob-avy))
 
 (use-package apheleia
   :blackout " ⚘"
