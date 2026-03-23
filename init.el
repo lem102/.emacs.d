93acbeed86261558eecb7c67223f1265678dee6f
lem102 <jacobtophatleeming@gmail.com>
2026-03-23 10:45:48 +0000

    patch consult-symbol to use describe-keymap where appropriate

--- init.el
+++ init.el
@@ -1009,6 +1009,28 @@ then remove this function from `find-file-hook'."
         consult-source-buffer (plist-put consult-source-buffer
                                          :state #'jacob-consult-buffer-state-no-tramp)))
 
+(use-package consult-symbol
+  :config
+  (defun jacob-consult-symbol--default-action (sym)
+    "Default action for selected symbol SYM.
+Uses `customize-group' for pure custom groups, `describe-face' for
+pure faces, `helpful-symbol' when available, and `describe-symbol'
+as fallback.
+
+jacob:
+Patched so that `describe-keymap' is used when symbol is a keymap."
+    (cond
+     ((and (get sym 'group-documentation) (not (fboundp sym)) (not (boundp sym)))
+      (customize-group sym))
+     ((and (facep sym) (not (fboundp sym)) (not (boundp sym)))
+      (describe-face sym))
+     ((keymapp (symbol-value sym))
+      (describe-keymap sym))
+     ((fboundp 'helpful-symbol) (helpful-symbol sym))
+     (t (describe-symbol sym))))
+
+  (advice-add #'consult-symbol--default-action :override #'jacob-consult-symbol--default-action))
+
 (use-package embark
   :defer t
   :bind
