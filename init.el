0c58c9f939fbda2691a3f8725419e0da462ade8a
lem102 <jacobtophatleeming@gmail.com>
2026-03-25 16:29:57 +0000

    add markdown to used treesit auto langs

--- init.el
+++ init.el
@@ -871,7 +871,7 @@ then remove this function from `find-file-hook'."
                                           :url "https://github.com/PrestonKnopp/tree-sitter-gdscript.git"
                                           :ext "\\.gd\\'"))
 
-  (setq treesit-auto-langs '(c-sharp scala yaml gdscript json))
+  (setq treesit-auto-langs '(c-sharp scala yaml gdscript json markdown))
   (treesit-auto-add-to-auto-mode-alist)
   (global-treesit-auto-mode 1))
 
