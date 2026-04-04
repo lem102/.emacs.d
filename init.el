84b34ce5b58087c1082af63b2c3aa1b855c88ea5
user <user@debianThinkpad>
2026-04-04 07:12:09 +0100

    undo llm suggestion >:)

--- init.el
+++ init.el
@@ -1118,7 +1118,17 @@ Patched so that `describe-keymap' is used when symbol is a keymap."
 (use-package sql-indent
   :hook (sql-mode-hook . sqlind-minor-mode))
 
-(require 'jacob-llm)
+(use-package gptel
+  :defer t
+  :custom
+  (gptel-default-mode #'org-mode)
+  (gptel-confirm-tool-calls t))
+
+(use-package mcp
+  :after gptel)
+
+(use-package elisp-dev-mcp
+  :after gptel)
 
 (use-package gdscript-mode
   :hook ((gdscript-ts-mode-hook . apheleia-mode)
