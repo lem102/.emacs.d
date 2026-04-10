49d12c25959941eaaacf625c6af311816e057fbc
Jacob Leeming <jacobtophatleeming@gmail.com>
2026-04-10 07:18:33 +0100

    create functions to install builtin packages

--- init.el
+++ init.el
@@ -1298,6 +1298,24 @@ For use with GitLab only."
     ;; (shell-command "git push")
     (shell-command "git stash pop")))
 
+(defun jacob-package-upgrade-only-builtins ()
+  "Upgrade all built-in packages to their latest versions."
+  (interactive)
+  (package-refresh-contents)
+  (dolist (pkg-name package-activated-list)
+    (when (and (package-built-in-p pkg-name)
+               (jacob-package-upgradeable-p pkg-name))
+      (message "Upgrading built-in: %s" pkg-name)
+      (package-install pkg-name))))
+
+(defun jacob-package-upgradeable-p (pkg-name)
+  "Return non-nil if PKG-NAME has a newer version available in archives."
+  (let* ((available-pkg (cadr (assq pkg-name package-archive-contents)))
+         (installed-pkg (cadr (assq pkg-name package-alist))))
+    (when (and available-pkg installed-pkg)
+      (version-list-< (package-desc-version installed-pkg)
+                      (package-desc-version available-pkg)))))
+
 (provide 'init)
 
 ;;; init.el ends here
