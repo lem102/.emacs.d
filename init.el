af376b27457f9fcc54ae25eb38224bcb96ee61ce
lem102 <jacobtophatleeming@gmail.com>
2026-04-16 23:19:16 +0100

    move gitlab code to own file

--- init.el
+++ init.el
@@ -1261,36 +1261,6 @@ Otherwise, display error message."
         (other-window 1))
     (message "More/less than 2 windows in frame.")))
 
-(defvar jacob-gitlab-push-set-upstream-jira-url ""
-  "URL for current employer's jira board.")
-
-(defun jacob-gitlab-push-set-upstream ()
-  "Push the current branch and create an upstream branch.
-Use GitLab push options to create a merge request and set all
-necessary values.
-
-For use with GitLab only."
-  (interactive)
-  (let* ((branch-name (with-temp-buffer
-                        (eshell-command "git symbolic-ref HEAD --short" t)
-                        (buffer-substring-no-properties (point-min) (- (point-max) 1))))
-         (mr-key (progn
-                   (string-match (rx (+ alpha) "-" (+ digit))
-                                 branch-name)
-                   (match-string 0 branch-name)))
-         (jira-link (concat jacob-gitlab-push-set-upstream-jira-url mr-key))
-         (command (concat "git push --set-upstream origin HEAD "
-                          (concat "-o merge_request.create "
-                                  "-o merge_request.remove_source_branch "
-                                  (concat "-o merge_request.title=\""
-                                          branch-name
-                                          "\" ")
-                                  (concat "-o merge_request.description=\""
-                                          "[" mr-key "](" jira-link ")"
-                                          "\" ")))))
-    (with-temp-buffer
-      (eshell-command command t))))
-
 (defun jacob-update-config ()
   "Update your Emacs configuration with git."
   (interactive)
---
+++ lisp/jacob-gitlab.el
@@ -0,0 +1,40 @@
+;;; jacob-gitlab.el --- Utilities for gitlab + jira  -*- lexical-binding: t; -*-
+
+;;; Commentary:
+;; 
+
+;;; Code:
+
+(defvar jacob-gitlab-push-set-upstream-jira-url ""
+  "URL for current employer's jira board.")
+
+(defun jacob-gitlab-push-set-upstream ()
+  "Push the current branch and create an upstream branch.
+Use GitLab push options to create a merge request and set all
+necessary values.
+
+For use with GitLab only."
+  (interactive)
+  (let* ((branch-name (with-temp-buffer
+                        (eshell-command "git symbolic-ref HEAD --short" t)
+                        (buffer-substring-no-properties (point-min) (- (point-max) 1))))
+         (mr-key (progn
+                   (string-match (rx (+ alpha) "-" (+ digit))
+                                 branch-name)
+                   (match-string 0 branch-name)))
+         (jira-link (concat jacob-gitlab-push-set-upstream-jira-url mr-key))
+         (command (concat "git push --set-upstream origin HEAD "
+                          (concat "-o merge_request.create "
+                                  "-o merge_request.remove_source_branch "
+                                  (concat "-o merge_request.title=\""
+                                          branch-name
+                                          "\" ")
+                                  (concat "-o merge_request.description=\""
+                                          "[" mr-key "](" jira-link ")"
+                                          "\" ")))))
+    (with-temp-buffer
+      (eshell-command command t))))
+
+(provide 'jacob-gitlab)
+
+;;; jacob-gitlab.el ends here
