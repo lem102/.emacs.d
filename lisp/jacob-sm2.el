fd120f4592a242bbe93a4317fadd4a2896ca79a7
lem102 <jacobtophatleeming@gmail.com>
2026-04-28 10:11:11 +0100

    add the jacob-sm2 library

---
+++ lisp/jacob-sm2.el
@@ -0,0 +1,45 @@
+;;; jacob-sm2.el --- Library for interacting with sm2  -*- lexical-binding: t; -*-
+
+;;; Commentary:
+;;
+
+;;; Code:
+
+(defcustom jacob-sm2-config-directory "~/.sm2/service-manager-config/"
+  "Directory where sm2 stores it's configuration.")
+
+(transient-define-prefix jacob-sm2 nil "Transient menu for sm2."
+  ["Commands"
+   ("i" "Status" jacob-sm2-status)
+   ("s" "Start" jacob-sm2-start)])
+
+(defun jacob-sm2-status ()
+  "Run sm2 -s."
+  (interactive)
+  (async-shell-command "sm2 -s"))
+
+(defun jacob-sm2-start ()
+  "Run sm2 --start. Prompt for which service or profile should be started."
+  (interactive)
+  (async-shell-command (format "sm2 --start %s"
+                               (completing-read "Service or profile: "
+                                                (jacob-sm2-services-and-profiles)))))
+
+(defun jacob-sm2-services-and-profiles ()
+  "Get all sm2 services and profiles."
+  (append (mapcar #'car
+                  (json-read-file (file-name-concat jacob-sm2-config-directory
+                                                    "profiles.json")))
+          (mapcar #'car
+                  (json-read-file (file-name-concat jacob-sm2-config-directory
+                                                    "services.json")))
+          (mapcar #'car
+                  (apply #'append
+                         (mapcar #'json-read-file
+                                 (directory-files (file-name-concat jacob-sm2-config-directory "services")
+                                                  "FULL"
+                                                  "^[^.]"))))))
+
+(provide 'jacob-sm2)
+
+;;; jacob-sm2.el ends here
