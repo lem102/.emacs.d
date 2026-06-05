af0cbdd61ec6d8dfa94e6096c7df267926d139a2
lem102 <jacobtophatleeming@gmail.com>
2026-06-05 08:25:15 +0100

    pull dired related use-package declarations into main file

--- init.el
+++ init.el
@@ -554,7 +554,35 @@ $0`(yas-escape-text yas-selected-text)`"))
                     (web-mode-block-previous)
                     (setq offset (current-indentation))))))
 
-(require 'jacob-dired)
+(use-package ls-lisp
+  :defer t
+  :init
+  (setq ls-lisp-use-insert-directory-program nil
+        ls-lisp-dirs-first t))
+
+(use-package dired
+  :defer t
+  :init
+  (add-hook 'dired-mode-hook #'dired-hide-details-mode)
+  (add-hook 'dired-mode-hook #'auto-revert-mode)
+  :config
+  (require 'dired-x)
+  (setopt dired-recursive-copies 'always
+          dired-dwim-target t
+          dired-listing-switches "-hal" ; the h option needs to come first 🙃
+          dired-guess-shell-alist-user '(("\\.mkv\\'" "mpv")
+                                         ("\\.mp4\\'" "mpv"))))
+
+(use-package dired-aux
+  :defer t
+  :config
+  (setopt dired-vc-rename-file t))
+
+(use-package dired-rsync
+  :defer t
+  :after dired
+  :config
+  (add-to-list 'mode-line-misc-info '(:eval dired-rsync-modeline-status 'append)))
 
 (use-package nerd-icons-dired
   :blackout
