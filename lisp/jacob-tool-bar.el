bd25804c8228566b5556fa9e4f6344496f0b1f61
Jacob Leeming <jacobtophatleeming@gmail.com>
2026-07-21 08:41:06 +0100

    add more buttons

--- lisp/jacob-tool-bar.el
+++ lisp/jacob-tool-bar.el
@@ -10,13 +10,30 @@
   (setq tool-bar-map (make-sparse-keymap))
   (setq secondary-tool-bar-map (make-keymap))
 
+  (jacob-tool-bar-add-top-item "spell" #'ignore 'jacob-ignore1)
+  (jacob-tool-bar-add-top-item "spell" #'ignore 'jacob-ignore2)
+  (jacob-tool-bar-add-top-item "spell" #'ignore 'jacob-ignore3)
+
   (jacob-tool-bar-add-top-item "left-arrow" #'backward-char 'jacob-left)
   (jacob-tool-bar-add-top-item "sort-ascending" #'next-line 'jacob-down)
   (jacob-tool-bar-add-top-item "right-arrow" #'forward-char 'jacob-right)
 
-  (jacob-tool-bar-add-bottom-item "prev-node" #'puni-backward-sexp 'jacob-backward-sexp)
+  (jacob-tool-bar-add-top-item "spell" #'ignore 'jacob-ignore4)
+  (jacob-tool-bar-add-top-item "spell" #'ignore 'jacob-ignore5)
+  (jacob-tool-bar-add-top-item "spell" #'ignore 'jacob-ignore6)
+
+
+  (jacob-tool-bar-add-bottom-item "spell" #'ignore 'jacob-ignore7)
+  (jacob-tool-bar-add-bottom-item "spell" #'ignore 'jacob-ignore8)
+  (jacob-tool-bar-add-bottom-item "spell" #'ignore 'jacob-ignore9)
+
+  (jacob-tool-bar-add-bottom-item "prev-node" #'puni-backward-sexp-or-up-list 'jacob-backward-sexp)
   (jacob-tool-bar-add-bottom-item "up-arrow" #'previous-line 'jacob-up)
-  (jacob-tool-bar-add-bottom-item "next-node" #'puni-forward-sexp 'jacob-forward-sexp))
+  (jacob-tool-bar-add-bottom-item "next-node" #'puni-forward-sexp-or-up-list 'jacob-forward-sexp)
+
+  (jacob-tool-bar-add-bottom-item "spell" #'ignore 'jacob-ignore10)
+  (jacob-tool-bar-add-bottom-item "spell" #'ignore 'jacob-ignore11)
+  (jacob-tool-bar-add-bottom-item "spell" #'ignore 'jacob-ignore12))
 
 (defun jacob-tool-bar-add-top-item (icon def key &rest props)
   "Add an item to top tool bar.
