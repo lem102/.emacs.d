2ad6a1201b2392e24bbd1baeeb0693a8558c83c4
Jacob Leeming <jacobtophatleeming@gmail.com>
2026-07-22 23:51:57 +0100

    add down arrow svg

--- lisp/jacob-tool-bar.el
+++ lisp/jacob-tool-bar.el
@@ -7,33 +7,45 @@
 
 (defun jacob-setup-tool-bars ()
   "Setup tool bars."
-  (setq tool-bar-map (make-sparse-keymap))
-  (setq secondary-tool-bar-map (make-keymap))
+  (let* ((down-arrow (let* ((svg (svg-create 25 25)))
+                       (svg-polygon svg
+                                    '((10 . 2)
+                                      (15 . 2)
+                                      (15 . 14)
+                                      (20 . 14)
+                                      (12 . 22)
+                                      (5 . 14)
+                                      (10 . 14))
+                                    :stroke-color "black"
+                                    :fill-color "green")
+                       (svg-image svg))))
+    (setq tool-bar-map (make-sparse-keymap))
+    (setq secondary-tool-bar-map (make-keymap))
 
-  (jacob-tool-bar-add-top-item "spell" #'ignore 'jacob-ignore1)
-  (jacob-tool-bar-add-top-item "spell" #'ignore 'jacob-ignore2)
-  (jacob-tool-bar-add-top-item "spell" #'ignore 'jacob-ignore3)
+    (jacob-tool-bar-add-top-item "spell" #'ignore 'jacob-ignore1)
+    (jacob-tool-bar-add-top-item "spell" #'ignore 'jacob-ignore2)
+    (jacob-tool-bar-add-top-item "spell" #'ignore 'jacob-ignore3)
 
-  (jacob-tool-bar-add-top-item "prev-node" #'puni-backward-sexp-or-up-list 'jacob-backward-sexp)
-  (jacob-tool-bar-add-top-item "up-arrow" #'previous-line 'jacob-up)
-  (jacob-tool-bar-add-top-item "next-node" #'puni-forward-sexp-or-up-list 'jacob-forward-sexp)
+    (jacob-tool-bar-add-top-item "prev-node" #'puni-backward-sexp-or-up-list 'jacob-backward-sexp)
+    (jacob-tool-bar-add-top-item "up-arrow" #'previous-line 'jacob-up)
+    (jacob-tool-bar-add-top-item "next-node" #'puni-forward-sexp-or-up-list 'jacob-forward-sexp)
 
-  (jacob-tool-bar-add-top-item "search" #'isearch-forward 'jacob-isearch)
-  (jacob-tool-bar-add-top-item "spell" #'ignore 'jacob-ignore5)
-  (jacob-tool-bar-add-top-item "spell" #'ignore 'jacob-ignore6)
+    (jacob-tool-bar-add-top-item "search" #'isearch-forward 'jacob-isearch)
+    (jacob-tool-bar-add-top-item "bookmark_add" #'expreg-expand 'jacob-mark)
+    (jacob-tool-bar-add-top-item "spell" #'ignore 'jacob-ignore6)
 
 
-  (jacob-tool-bar-add-bottom-item "undo" #'undo 'jacob-undo)
-  (jacob-tool-bar-add-bottom-item "cut" #'jacob-kill-line 'jacob-kill-line)
-  (jacob-tool-bar-add-bottom-item "paste" #'ignore 'jacob-ignore9)
+    (jacob-tool-bar-add-bottom-item "undo" #'undo 'jacob-undo)
+    (jacob-tool-bar-add-bottom-item "cut" #'jacob-kill-line 'jacob-kill-line)
+    (jacob-tool-bar-add-bottom-item "paste" #'yank 'jacob-yank)
 
-  (jacob-tool-bar-add-bottom-item "left-arrow" #'backward-char 'jacob-left)
-  (jacob-tool-bar-add-bottom-item "sort-ascending" #'next-line 'jacob-down)
-  (jacob-tool-bar-add-bottom-item "right-arrow" #'forward-char 'jacob-right)
+    (jacob-tool-bar-add-bottom-item "left-arrow" #'backward-char 'jacob-left)
+    (jacob-tool-bar-add-bottom-item "placeholder" #'next-line 'jacob-down :image down-arrow)
+    (jacob-tool-bar-add-bottom-item "right-arrow" #'forward-char 'jacob-right)
 
-  (jacob-tool-bar-add-bottom-item "spell" #'ignore 'jacob-ignore10)
-  (jacob-tool-bar-add-bottom-item "spell" #'ignore 'jacob-ignore11)
-  (jacob-tool-bar-add-bottom-item "spell" #'ignore 'jacob-ignore12))
+    (jacob-tool-bar-add-bottom-item "spell" #'ignore 'jacob-ignore10)
+    (jacob-tool-bar-add-bottom-item "spell" #'ignore 'jacob-ignore11)
+    (jacob-tool-bar-add-bottom-item "spell" #'ignore 'jacob-ignore12)))
 
 (defun jacob-tool-bar-add-top-item (icon def key &rest props)
   "Add an item to top tool bar.
